ca1f7b541d8b3c15246cea83fb956e1e7b38c044
massoprod <patrik@kontentino.com>
2020-04-21 16:09:52 +0200

    Updates

---
+++ precache-manifest.439bb73d8eaa8398197c1ed7e8881567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81822c0f337e7295758eb8436e66b91",
+    "url": "/instagram-insights/index.html"
+  },
+  {
+    "revision": "7caaba6a433faf7abb49",
+    "url": "/instagram-insights/static/css/main.3bd3bab1.chunk.css"
+  },
+  {
+    "revision": "56d31f65c2f3a6886db4",
+    "url": "/instagram-insights/static/js/2.0a339297.chunk.js"
+  },
+  {
+    "revision": "7caaba6a433faf7abb49",
+    "url": "/instagram-insights/static/js/main.9935f6e8.chunk.js"
+  },
+  {
+    "revision": "3f310ccd8e6a58f68971",
+    "url": "/instagram-insights/static/js/runtime-main.0652f22e.js"
+  },
+  {
+    "revision": "6517a7ee758bff798a9b265dd38562a5",
+    "url": "/instagram-insights/static/media/instagram-logo.6517a7ee.jpeg"
+  }
+]);
